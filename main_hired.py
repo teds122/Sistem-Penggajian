a72c0d809ef1f3ecd26bf46c931f077ca6fb8850
teds122 <119777864+teds122@users.noreply.github.com>
2023-05-22 13:25:41 +0700

    main_hired.py

--- main_hired.py
+++ main_hired.py
@@ -1,140 +1,68 @@
 import streamlit as st
-import pandas as pd
-import numpy as np 
-from collections import Counter
-import plotly.express as px
-import math
-import webbrowser
+import sqlite3
 
-def wide(): 
-    st.set_page_config(layout="wide")
-wide()
+# Membuat koneksi ke database
+conn = sqlite3.connect('payroll.db')
 
+# Membuat kursor
+cursor = conn.cursor()
 
-header = st.container()
-data_and_selection = st.container()
-graphs = st.container()
-hist = st.container()
+# Membuat tabel jika belum ada
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS employees (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        position TEXT NOT NULL,
+        salary INTEGER NOT NULL
+    )
+''')
 
-@st.cache
-def get_data(filename):
-    data = pd.read_csv(filename)
-    return data
-    
-#header in Ukr.
-with header: 
+# Fungsi untuk menambahkan data pegawai
+def add_employee(name, position, salary):
+    cursor.execute("INSERT INTO employees (name, position, salary) VALUES (?, ?, ?)", (name, position, salary))
+    conn.commit()
 
-    st.title('Salary and Skills of Hired Candidates') #Most Popular Skills Among
-    st.markdown('We analyzed data from the profiles of **12K candidates** who were hired on Djinni since March 2022. \
-         Select a category and experience below to see the key skills and salaries these candidates have listed in their profiles.' )
+# Fungsi untuk mengambil data pegawai
+def get_employees():
+    cursor.execute("SELECT * FROM employees")
+    rows = cursor.fetchall()
+    return rows
 
-    url = 'https://djinni.co/hire'
-    btn = st.button('Find a job on Djinni :genie:')
-    if btn:
-        webbrowser.open_new_tab(url)
+# Fungsi untuk menghitung total gaji
+def calculate_total_salary():
+    cursor.execute("SELECT SUM(salary) FROM employees")
+    total_salary = cursor.fetchone()[0]
+    return total_salary
 
-with data_and_selection: 
-    st.subheader('Choose the category and experience')
-#loading data
-    cand = get_data('skills_hired_cand.csv')
-    cand = cand.dropna()
-    cand.rename(columns = {'skills_cache':'all_skils'}, inplace=True)
+# Tampilan aplikasi dengan Streamlit
+def main():
+    st.title("Aplikasi Penggajian")
 
-    cat_col, exp_col = st.columns(2, gap = 'medium')
+    # Menambahkan data pegawai
+    st.header("Tambah Pegawai")
+    emp_name = st.text_input("Nama Pegawai")
+    emp_position = st.text_input("Posisi Pegawai")
+    emp_salary = st.number_input("Gaji Pegawai", min_value=0)
+    if st.button("Tambah"):
+        add_employee(emp_name, emp_position, emp_salary)
+        st.success("Pegawai ditambahkan!")
 
- #the dropdown. value = '' (defealt)
-    options = cand['primary_keyword'].unique()
-    options = np.insert(options,0,'')
-    options = np.sort(options)
-    keyword = cat_col.selectbox('Empty choice shows all the categories together', options = options, index =0) 
+    # Menampilkan data pegawai
+    st.header("Data Pegawai")
+    employees = get_employees()
+    for emp in employees:
+        st.write(f"Nama: {emp[1]}")
+        st.write(f"Posisi: {emp[2]}")
+        st.write(f"Gaji: {emp[3]}")
+        st.write("---")
 
-#experience range
-    m = float(cand.experience_years.min())
-    x = float(cand.experience_years.max())
+    # Menampilkan total gaji
+    total_salary = calculate_total_salary()
+    st.header("Total Gaji")
+    st.write(f"Total: {total_salary}")
 
-    exp_range = exp_col.slider('Here you can narrow the experience of hired candidates', value = [m,x], step = 0.5)
-    
-# the function takes data and category and returns a list of skills in category in order of popularity 
-    def one_list(data, keyword='', exp=exp_range):
-        a = data[(data.experience_years <= exp[1]) & (data.experience_years >= exp[0])]
-        if keyword: 
-            a = a[a['primary_keyword'] == keyword]
-        
-        skils = a['all_skils']
-        list = skils.tolist()
-        all_skils = []
-        for i in range(len(list)):
-            a = list[i]
-            a = a.split('\n')
-            all_skils.append(a)
-        flat_listt = [item for sublist in all_skils for item in sublist]
-        flat_list =[]
-        for skill in flat_listt: 
-            skill = skill.strip()
-            if skill.lower() != keyword.lower():
-                flat_list.append(skill)
-        myDict = Counter(flat_list)
+if __name__ == "__main__":
+    main()
 
-        skillshare = pd.DataFrame.from_dict(myDict, orient='index')
-        skillshare['skill'] = skillshare.index
-        skillshare.rename(columns = {0:'count'}, inplace=True)
-        skillshare = skillshare[['skill', 'count']]
-        skillshare = skillshare.reset_index(drop=True)
-        skillshare = skillshare.sort_values(by='count', ascending=False).head(15)
-        skillshare = skillshare.sort_values(by='count', ascending=True)
-        
-        return skillshare
-
-with graphs:
-
-    if keyword == '':
-        filted = cand[(cand.experience_years <=exp_range[1]) & (cand.experience_years >=exp_range[0])]
-        
-    else:
-        filted = cand[(cand['primary_keyword'] == keyword)&(cand.experience_years <=exp_range[1]) & (cand.experience_years >=exp_range[0])]
-    
-    #execute the function to know the skills
-    cand_skills = one_list(cand, keyword = keyword, exp =exp_range)
-    num_cand = filted.shape[0]
-    cand_skills['Share of candidates'] = cand_skills['count']*1./num_cand
-
-    top = cand_skills.sort_values(by='count', ascending=False).head(1).reset_index()
-    pct = round(top['Share of candidates'][0]*100,0)
-
-    if keyword:
-        cat = keyword
-    else:
-        cat = 'all hired'
-
-    st.subheader('{} is the most popular skill among {} candidates. {}% mentioned it.'.format(top.skill[0], cat, pct))
-
-    st.markdown('Candidates in selection: {}'.format(num_cand))
-    
-
-    fig = px.bar(cand_skills, x="Share of candidates", y="skill", orientation='h', labels={"skill": "Top Skills", 'Share of candidates':'Share of candidates with the skill'},)
-
-    fig.update_yaxes(tickfont=dict(size=12))
-    fig.update_xaxes(tickfont=dict(size=12))
-    fig.update_layout(height=5000)
-
-    st.plotly_chart(fig)
-    
-with hist:
-    med = filted['salary_min'].median()
-    filted = filted[filted['salary_min']<=17000]
-    st.subheader('{} is the median salary among hired {} candidates with {} - {} years of experience'\
-                 .format(med, keyword, exp_range[0],exp_range[1] ))
-    
-    bin_width= 230
-    # here you can choose your rounding method, I've chosen math.ceil
-    nbins = math.ceil((filted["salary_min"].max() - filted["salary_min"].min()) / bin_width)
-     
-
-    h_fig = px.histogram(filted, x = 'salary_min', nbins=nbins,
-                         labels={"salary_min": "Salary in profile"},
-                         color_discrete_sequence=['#432092 '],
-                         title="Salary distribution")
-    h_fig.update_traces(marker_line_width=2,marker_line_color="white")
-    h_fig.layout.yaxis.title.text = 'Candidates hired'
-
-    st.plotly_chart(h_fig)
+# Menutup koneksi
+conn.close()
